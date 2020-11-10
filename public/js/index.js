5af6529cecc0b31d27e394269c9a4a96cfd63c27
qiujiujiu <QGG929685098@163.com>
2020-11-10 14:03:06 +0800

    11/10提交--v1.0.1

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+import './libray/jquery.js';
+import './libray/jquery-banner.js';
+
+
+$(".swiper-container").carousel({
+    width: 1230,
+    height: 500,
+    speed: 5000,
+    needPrevAndNextBtnGroup: true,
+    needPagination: true
+});
